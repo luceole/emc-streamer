c7e2cc5615e0d1eac579ac501fa6989126df2249
luc Bourdot <luc.bourdot@ac-dijon.fr>
2022-02-02 23:39:11 +0100

    WIP: Fix Embed

--- routes/yt.js
+++ routes/yt.js
@@ -1,6 +1,7 @@
 // Videos On YouTube
 const cp = require('child_process');
 const readline = require('readline');
+const axios = require('axios')
 var config = require('config');
 var vstream = require("../vstream.js");
 var fs = require("fs");
@@ -9,76 +10,61 @@ var ytdl = require("ytdl-core");
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegStatic = require('ffmpeg-static');
 var url = require("url");
-const {
-  pipe
-} = require("superagent");
+
 
 var rootFolder = config.serveIndex.rootFolder;
 console.log(rootFolder)
 
 
-exports.convertdownload = function (req,res) {
+exports.convertdownload = function (req, res) {
   var refUrl = "http://www.youtube.com/watch?v=" + req.params.idVideo;
   res.setHeader('Connection', 'Transfer-Encoding');
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.setHeader('Transfer-Encoding', 'chunked');
-
   res.write('<html> Convertion et Téléchargement  ');
   if (ytdl.validateID(req.params.idVideo)) {
-    
     try {
       ytdl.getInfo(req.params.idVideo).then((infos) => {
         let stream = ytdl(refUrl, {
           quality: 'highestaudio',
         });
-        res.write('<br> '+ infos.videoDetails.title )
+        res.write('<br> ' + infos.videoDetails.title)
         res.write('<br> Conversion audio => MP3 ...')
-      
         let start = Date.now();
         ffmpeg(stream)
           .audioBitrate(128)
-         .save(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp3`)
+          .save(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp3`)
           .on('progress', p => {
             //readline.cursorTo(process.stdout, 0);
-           // process.stdout.write(`${p.targetSize}kb downloaded`);
+            // process.stdout.write(`${p.targetSize}kb downloaded`);
           })
           .on('end', () => {
-            
             res.write(`     OK - ${(Date.now() - start) / 1000}s`);
             res.write("<br> Remix Audio et Vidéo => MP4...")
             video = ytdl(refUrl);
             var proc = ffmpeg(video)
-            .addOutputOptions('-movflags +frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov')
-            //.audioBitrate(128)
-            .addInput(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp3`)
-            //.addInput(audio)
-            .on('end', function() {
-              res.write(`     OK - ${(Date.now() - start) / 1000}s <br><a href="/medias/${infos.videoDetails.title}.mp4">  Regarder maintenant! </a>`);
-              res.end("</html>")
-            })
-            .on('error', function(err) {
-              console.log('an error happened: ' + err.message);
-            })  
-            //.pipe(res, {end:true})
-            .save(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp4`)
-
-
-
+              .addOutputOptions('-movflags +frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov')
+              //.audioBitrate(128)
+              .addInput(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp3`)
+              //.addInput(audio)
+              .on('end', function () {
+                res.write(`     OK - ${(Date.now() - start) / 1000}s <br><a href="/medias/${infos.videoDetails.title}.mp4">  Regarder maintenant! </a>`);
+                res.end("</html>")
+              })
+              .on('error', function (err) {
+                console.log('an error happened: ' + err.message);
+              })
+              //.pipe(res, {end:true})
+              .save(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp4`)
           })
-
       });
     } catch (error) {
       //console.error(error);
       res.send("Video Not Found");
     }
-   
-
   } else {
     res.send("Video Not Found");
   }
-
-
-
 }
 /*
  * Download on server
@@ -88,28 +74,21 @@ exports.serverdownloadmp3 = function (req, res) {
   res.setHeader('Connection', 'Transfer-Encoding');
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.setHeader('Transfer-Encoding', 'chunked');
-
   res.write("<html> Conversion audio MP3")
   if (ytdl.validateID(req.params.idVideo)) {
-    
     try {
       ytdl.getInfo(req.params.idVideo).then((infos) => {
         let stream = ytdl("http://www.youtube.com/watch?v=" + req.params.idVideo, {
           quality: 'highestaudio',
         });
-        res.write('<br>'+infos.videoDetails.title)
+        res.write('<br>' + infos.videoDetails.title)
         res.write('<br> Conversion audio => MP3 ...')
         let start = Date.now();
         ffmpeg(stream)
           .audioBitrate(128)
-         .save(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp3`)
-          .on('progress', p => {
-            //readline.cursorTo(process.stdout, 0);
-           // process.stdout.write(`${p.targetSize}kb downloaded`);
-          })
+          .save(`${config.serveIndex.rootFolder}/${infos.videoDetails.title}.mp3`)
           .on('end', () => {
-            res.end(` OK- ${(Date.now() - start) / 1000}s <br><a href="/medias/${infos.videoDetails.title}.mp3"> Ecouter maintenant! </a>`); 
-           
+            res.end(` OK- ${(Date.now() - start) / 1000}s <br><a href="/medias/${infos.videoDetails.title}.mp3"> Ecouter maintenant! </a>`);
           })
 
       });
@@ -127,17 +106,14 @@ exports.serverdownload = function (req, res) {
     try {
       ytdl.getInfo(req.params.idVideo).then((infos) => {
         try {
-        const format = ytdl.chooseFormat(infos.formats, {
-          quality: '18'
-        });
-      }
-       catch (error) {
+          const format = ytdl.chooseFormat(infos.formats, {
+            quality: '18'
+          });
+        } catch (error) {
           res.send("Video Not Found");
           return;
         }
-      
         //console.log(format)
-
         video = ytdl("http://www.youtube.com/watch?v=" + req.params.idVideo)
         let starttime;
         video.pipe(fs.createWriteStream(config.serveIndex.rootFolder + '/' + infos.videoDetails.title + '.mp4'));
@@ -146,7 +122,6 @@ exports.serverdownload = function (req, res) {
           starttime = Date.now();
         });
         video.on('progress', (chunkLength, downloaded, total) => {
-
           const percent = downloaded / total;
           const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
           const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
@@ -158,7 +133,6 @@ exports.serverdownload = function (req, res) {
           // res.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
           //res.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `+'</body></html>');
           //readline.moveCursor(process.stdout, 0, -1);
-
         });
         video.on('end', () => {
           res.end(' <br><strong>terminé</strong>');
@@ -167,19 +141,16 @@ exports.serverdownload = function (req, res) {
     } catch (error) {
       console.error(error);
       res.send("Video Not Found");
-
     }
   } else {
     res.send("Video Not Found");
   }
-
 }
 /*
  * GET Video Info
  */
 
 exports.info = function (req, res) {
-
   if (ytdl.validateID(req.params.idVideo)) {
     try {
       ytdl.getInfo(req.params.idVideo).then((infos) => {
@@ -193,7 +164,6 @@ exports.info = function (req, res) {
     } catch (error) {
       console.error(error);
       res.send("Video Not Found");
-
     }
   } else {
     res.send("Video Not Found");
@@ -220,7 +190,6 @@ exports.geturl = function (req, res) {
     } catch (error) {
       console.error(error);
       res.send("Video Not Found");
-
     }
   } else {
     res.send("Video Not Found");
@@ -230,20 +199,18 @@ exports.geturl = function (req, res) {
 /*
  * STREAM video  WEBM ou MP4
  */
-
 exports.stream = function (req, res) {
   if (ytdl.validateID(req.params.idVideo)) {
     try {
       ytdl.getInfo(req.params.idVideo).then((infos) => {
         try {
-        const format = ytdl.chooseFormat(infos.formats, {
-          quality: 'highest'
-        });
-      }
-      catch (error) {
-        res.send("Video Not Found");
-        return;
-      }
+          const format = ytdl.chooseFormat(infos.formats, {
+            quality: 'highest'
+          });
+        } catch (error) {
+          res.send("Video Not Found");
+          return;
+        }
         //console.log(format.url)  
         mystream = ytdl("http://www.youtube.com/watch?v=" + req.params.idVideo, {
           filter: format => format.container === 'webm'
@@ -254,25 +221,27 @@ exports.stream = function (req, res) {
     } catch (error) {
       console.error(error);
       res.send("Video Not Found");
-
     }
   } else {
     res.send("Video Not Found");
   }
 };
+
+/*
+ * STREAM AUDIO MP3
+ */
 exports.audio = function (req, res) {
   if (ytdl.validateID(req.params.idVideo)) {
     try {
       ytdl.getInfo(req.params.idVideo).then((infos) => {
         try {
-        const format = ytdl.chooseFormat(infos.formats, {
-          quality: 'highest'
-        });
-      }
-      catch (error) {
-        res.send("Video Not Found")
-        return;
-      }
+          const format = ytdl.chooseFormat(infos.formats, {
+            quality: 'highest'
+          });
+        } catch (error) {
+          res.send("Video Not Found")
+          return;
+        }
         //console.log(format.url)  
         mystream = ytdl("http://www.youtube.com/watch?v=" + req.params.idVideo, {
           filter: 'audioonly'
@@ -283,20 +252,19 @@ exports.audio = function (req, res) {
     } catch (error) {
       console.error(error);
       res.send("Video Not Found");
-
     }
   } else {
     res.send("Video Not Found");
   }
-
 };
 
-
+/*
+ * Download on Server as local video
+ */
 exports.download = function (req, res) {
   videoID = req.params.idVideo;
   //ytdl.getInfo(videoID).then(info) => {
   //ytdl.getInfo(req.params.idVideo).then((info) => {
-
   //const format = ytdl.chooseFormat(info.formats, {quality: 'highest'});
   //console.log(format.url)  
   // RNFetchBlob
@@ -333,36 +301,69 @@ exports.download = function (req, res) {
     } catch (error) {
       console.error(error);
       res.send("Video Not Found");
-
     }
   } else {
     res.send("Video Not Found");
   }
-
 };
 
+/*
+ * GetOembeb
+ */
 exports.getoembed = function (req, res) {
   console.log("getOembed :" + req.params.idVideo);
   var url =
     "https://www.youtube.com/oembed?url=http%3A//www.youtube.com/watch%3Fv%3D" +
-    req.params.idVideo +
-    "&format=json";
-  //request(url).pipe(res);
-  request(url, function (error, reponse, body) {
-    var oembed = reponse.body;
-    res.send(req.query.callback + "(" + JSON.stringify(oembed) + ");");
-  });
+    req.params.idVideo + "&format=json";
+  axios
+    .get(url)
+    .then(result => {
+      // console.log(`statusCode: ${result.status}`)
+      // console.log(result.data)
+      //res.send(req.query.callback  + result.data.html.html );
+      //  res.send(req.query.callback + "(" + JSON.stringify(result.data) + ");");
+      request(url, (error, reponse) => {
+        var oembed = reponse.body;
+        res.send(req.query.callback + "(" + JSON.stringify(oembed) + ");");
+      });
+    })
+    .catch(error => {
+      res.send("Video Not Found");
+      return
+    })
 };
 
+/*
+ * Embed Video (iframe)
+ */
 exports.getembed = function (req, res) {
-  console.log("getOembed :" + req.params.idVideo);
+  console.log("getembed :" + req.params.idVideo);
   var url =
-    "https://www.youtube.com/oembed?url=http%3A//www.youtube.com/watch%3Fv%3D" +
+    "https://www.youtube.co/oembed?url=http%3A//www.youtube.com/watch%3Fv%3D" +
     req.params.idVideo +
     "&format=json";
-  request(url, function (error, reponse, body) {
-    obj = JSON.parse(reponse.body);
-    console.log(reponse.body);
-    res.send(obj.html);
-  });
+  axios
+    .get(url)
+    .then(result => {
+      //console.log(`statusCode: ${result.status}`)
+      //console.log(result)
+      res.send(result.data.html)
+    })
+    .catch(error => {
+      res.send("Video Not Found");
+    })
+
+  //     console.log(error)
+  //     if (error) {
+  //       res.send("Video Not Found ")
+  //     } else {
+  //     obj = JSON.parse(reponse.body);
+  //     console.log(reponse.body);
+  //     res.send(obj.html);
+  //     }
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  //  
+  // }
 };
